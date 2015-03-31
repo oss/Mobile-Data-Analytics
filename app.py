4472270da6427c0aba551ec916c2295653466de0
Indraneel Purohit <i.m.indraneel@gmail.com>
2015-03-31 14:53:14 -0400

    working on active users with threshold

--- app.py
+++ app.py
@@ -8,16 +8,13 @@ db = MySQLdb.connect(config.host,
 
 cur = db.cursor()
 
-def monthly_active_users(cur):
-    """
-	currently defining active as having registered at least one event
-    """
+def monthly_unique_users():
     cur.execute("SELECT COUNT(DISTINCT uuid) from analytics")
     print "monthly active"
     for row in cur.fetchall():
 	print row[0]
 
-def weekly_active_users(cur):
+def weekly_unique_users():
     weeks = ["2014-07-01", "2014-07-08", "2014-07-15", "2014-07-22", "2014-07-31"]
     print "start","end","\t","active"
     print "-----","---","\t","------"
@@ -29,7 +26,7 @@ def weekly_active_users(cur):
 	    print start,end,"\t",row[0]
 
 
-def daily_active_users(cur):
+def daily_unique_users():
     print "start","end","\t","active"
     print "-----","---","\t","------"
     for w in xrange(30):
@@ -39,7 +36,30 @@ def daily_active_users(cur):
 	for row in cur.fetchall():
 	    print start,end,"\t",row[0]
 
+def monthly_active_users(k):
+    """
+	k = minimum events to count as active
+    """
+    pass
 
-monthly_active_users(cur)
-weekly_active_users(cur)
-daily_active_users(cur)
+def daily_active_users(k):
+    users = {}
+    print "start\t","end","\t","active"
+    print "-----\t","---","\t","------"
+    for w in xrange(30):
+	daily_count = 0
+	start = "2014-07-"+str(w+1)
+	end = "2014-07-"+str(w+2)
+	cur.execute("SELECT uuid, handle from analytics where date between %s and %s", (start,end,))
+	for row in cur.fetchall():
+	    if row[0] not in users:
+		users[row[0]] = 0
+	    users[row[0]] += 1
+	    
+	    if users[row[0]] > k:
+		daily_count += 1
+		break
+	
+	print start, "\t", end, "\t", str(daily_count)
+
+daily_active_users(3)
